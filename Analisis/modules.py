fb29f9362e89457ebc1b7b519dfe0c7f3182872e
Mariana Nakagawa <nakagawamariana@gmail.com>
2020-10-26 21:46:16 +0100

    Add files via upload
    FALLO EN PRODUCTO ESCALAR

--- Analisis/modules.py
+++ Analisis/modules.py
@@ -41,7 +41,7 @@ def salpimienta(n_type,image,intensity):
 
 #plt.imshow(img)
 def mean_filter(img):
-    size_filter = 5
+    size_filter = 3
     # the filter is divided by size_filter^2 for normalization
     mean_filter = np.ones((size_filter,size_filter))/np.power(size_filter,2)
     # performing convolution
@@ -49,51 +49,92 @@ def mean_filter(img):
     return img_meanfiltered
 
 
-def median_filter(img):
-    img_filtered = filters.median_filter(img,size=5,mode='reflect')
+def median_filter(img, size):
+    img_filtered = filters.median_filter(img,size = size,mode='reflect')
     return img_filtered
 
-def gaussian_filter(img):
-
+def gaussian_filter(img,sigma):
     # performing convolution
-    img_gaussianfiltered = filters.gaussian_filter(img, sigma = 3,mode='reflect')
+    img_gaussianfiltered = filters.gaussian_filter(img, sigma=sigma,mode='reflect')
     return img_gaussianfiltered
 
-def submatrix(img, row, col):
 
-    for i in range(0,row-2):
+    
+#NLM: filtrar la imagen mediante el promedio ponderado de 
+#los diferentes píxeles de la imagen en función de su similitud
+#con el píxel original.
+
+def nlm(img, h_square):
+#padding 
+
+    img = np.pad(img,1, mode='reflect') #NO SE SI EL PADDING ESTA BIEN
+
+    
+    
+    row,col = img.shape
+    
+    
+    matriz_pesos = np.zeros(shape=(img.shape[0],img.shape[1])) #aqui almacenamos los pesos
+    
+    matriz_imagen = np.ones(shape=(img.shape[0],img.shape[1]))
+
+    for i in range(1,img.shape[0]-1):
         #print(i)
-        for j in range(0,col-2):
+        for j in range(1,img.shape[1]-1):
             #print(j)
             
             #parche 3x
             
-            matrix1 = np.array([[img[i-1,j-1],img[i-1,j],img[i-1,j+1]], 
+            matriz1 = np.array([[img[i-1,j-1],img[i-1,j],img[i-1,j+1]], 
                                 [img[i,j-1],img[i,j],img[i,j+1]], 
                                 [img[i+1,j-1],img[i+1,j],img[i+1,j+1]]])
             
-            #print(matrix1)
-    
-            
+            #print(matriz1)
             for x in range(0,row-2):
                 for y in range(0,col-2):
                     
-                    matrix2 = np.array([[img[x-1,y-1],img[x-1,y],img[x-1,y+1]], 
+                        matriz2 = np.array([[img[x-1,y-1],img[x-1,y],img[x-1,y+1]], 
                                 [img[x,y-1],img[x,j],img[x,y+1]], 
                                 [img[x+1,y-1],img[x+1,y],img[x+1,y+1]]])
                     
-                    distance = np.sqrt((matrix1-matrix2)**2)
-                    distance = np.sum(distance)
+                        distance = np.sqrt((matriz1-matriz2)**2)
+                        distance = np.sum(distance)
                     
-                   # print(matrix2)
+                        weights_ij = (np.exp(-distance/h_square))
+                    
+                        matriz_pesos[x,y] = weights_ij 
+                    
+                        #print(weights_ij)
+                    
+                    #print(matriz2)
                     
                     #print(distance)
-                    
-        return distance
-
+            
+            
+            matriz_ponderada = matriz_pesos/np.sum(matriz_pesos)
+            
+            #matriz_ponderada = np.squeeze(np.asarray(matriz_ponderada))
+            #img_gauss = np.squeeze(np.asarray(img_gauss))
+            
+            
+            matriz_imagen[i-1,j-1] = np.dot(img,np.transpose(matriz_ponderada),out=None)
+            #matriz_imagen[i-1,j-1] = np.transpose(img_gauss)* matriz_ponderada
+            
+            #img_gauss[i-1,j-1] = np.sum(matriz_pesos)
+            
+    return matriz_imagen
+    #img_nlm = np.dot(matriz_pesos,img_gauss) # ESTO DA ERROR 
+        
+        
     
     
+    
+    #img_nlm = NLM(img)
 
+
+
+
+                
 
 
 
