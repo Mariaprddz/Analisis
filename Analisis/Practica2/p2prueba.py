f84e3e05df111a5d97420d50e9d18ace6a72f503
Mariana Nakagawa <nakagawamariana@gmail.com>
2020-11-21 13:18:19 +0100

    Add files via upload

--- Analisis/Practica2/p2prueba.py
+++ Analisis/Practica2/p2prueba.py
@@ -63,64 +63,82 @@ coords = np.array([(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), 
 
     
 #%%
-#Pasar nuestra a un array
-pixels = np.array(seed)
 
-#crear una matriz de ceros
-region= np.zeros(shape=(img_o.shape[0],img_o.shape[1]))
-
-#guardar nuestro pixel semilla en la ROI y lo llevamos a blanco
-region[pixels[0],pixels[1]]=1
-#definimos variables de umbral 
-umbral_inf=0.1
-umbral_sup=0.1
-intervalo_inf = img_o[pixels[0],pixels[1]]-umbral_inf
-intervalo_sup = img_o[pixels[0],pixels[1]]+umbral_sup
-
-
-
+def RegionGrowingP2(img, umbral_inf, umbral_sup):
+    img = img_o
+    plt.imshow(img_o, cmap='gray')
+    click_markers = plt.ginput(n=1,timeout=30)
+    print(click_markers[0])
+    click_markers = list(click_markers[0])
+    print(click_markers)
+         
+    markers = [round(num) for num in click_markers ]
+    seed = [markers[1],markers[0]] 
+     
     
-
-print (img_o.shape)                
-
-for x in range (0, coords.shape[0]):
-
-    if intervalo_inf<=img_o[pixels[0]+coords[x,0], pixels[1]+coords[x,1]]<=intervalo_sup :
-                            
-        region[pixels[0]+coords[x,0], pixels[1]+coords[x,1]] = 1      
+    print(seed)
+    
+    coords = np.array([(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)])
+    
+    #Pasar nuestra a un array
+    pixels = np.array(seed)
+    
+    #crear una matriz de ceros
+    region= np.zeros(shape=(img_o.shape[0],img_o.shape[1]))
+    
+    #guardar nuestro pixel semilla en la ROI y lo llevamos a blanco
+    region[pixels[0],pixels[1]]=1
+    #definimos variables de umbral 
+    #umbral_inf=0.1
+    #umbral_sup=0.1
+    intervalo_inf = img_o[pixels[0],pixels[1]]-umbral_inf
+    intervalo_sup = img_o[pixels[0],pixels[1]]+umbral_sup
+    
+    
+    
         
-    else:
-        pass
-                
-new_pix = np.where(region == 1)
-Coordinates = np.array(list(zip(new_pix[0], new_pix[1])))
-listOfCoordinates = list(zip(new_pix[0], new_pix[1]))
-regionCoords =[]
-print(len (listOfCoordinates),len(regionCoords))
+    
+    print (img_o.shape)                
+    
+    for x in range (0, coords.shape[0]):
+    
+        if intervalo_inf<=img_o[pixels[0]+coords[x,0], pixels[1]+coords[x,1]]<=intervalo_sup :
+                                
+            region[pixels[0]+coords[x,0], pixels[1]+coords[x,1]] = 1      
             
-while len (listOfCoordinates)!=len(regionCoords):
+        else:
+            pass
+                    
     new_pix = np.where(region == 1)
-    Coordinates = np.array(list(zip(new_pix[0], new_pix[1]))) 
+    Coordinates = np.array(list(zip(new_pix[0], new_pix[1])))
     listOfCoordinates = list(zip(new_pix[0], new_pix[1]))
+    regionCoords =[]
     
-    for i in range (0, Coordinates.shape[0]):
-    
-            for x in range (0, 8):
-                if Coordinates[i,0]+coords[x,0] > 0 and Coordinates[i,1]+coords[x,1]>0 and Coordinates[i,0]+coords[x,0]<img_o.shape[0] and Coordinates[i,1]+coords[x,1]<img_o.shape[1]:
-                    if intervalo_inf<=img_o[Coordinates[i,0]+coords[x,0], Coordinates[i,1]+coords[x,1]]<=intervalo_sup :
-                                    
-                        region[Coordinates[i,0]+coords[x,0], Coordinates[i,1]+coords[x,1]] = 1      
-                        
+                
+    while len (listOfCoordinates)!=len(regionCoords):
+        new_pix = np.where(region == 1)
+        Coordinates = np.array(list(zip(new_pix[0], new_pix[1]))) 
+        listOfCoordinates = list(zip(new_pix[0], new_pix[1]))
+        
+        for i in range (0, Coordinates.shape[0]):
+        
+                for x in range (0, 8):
+                    if Coordinates[i,0]+coords[x,0] >= 0 and Coordinates[i,1]+coords[x,1]>= 0 and Coordinates[i,0]+coords[x,0]<img_o.shape[0] and Coordinates[i,1]+coords[x,1]<img_o.shape[1]:
+                        if intervalo_inf<=img_o[Coordinates[i,0]+coords[x,0], Coordinates[i,1]+coords[x,1]]<=intervalo_sup :
+                                        
+                            region[Coordinates[i,0]+coords[x,0], Coordinates[i,1]+coords[x,1]] = 1      
+                            
+                        else:
+                            pass
                     else:
                         pass
-                else:
-                    pass
-    regionCoords = np.where(region == 1)
-    regionCoords = list(zip(regionCoords[0], regionCoords[1]))
-    print(len (listOfCoordinates),len(regionCoords))
+        regionCoords = np.where(region == 1)
+        regionCoords = list(zip(regionCoords[0], regionCoords[1]))
+
     
-    #return region            
-                                                                                            
+    return region            
+
+region = RegionGrowingP2(img_o, 0.3, 0.1)                                                                                            
 plt.figure()
 plt.title('Mascarita puñetera de 1')
 plt.imshow(region, cmap=plt.cm.gray)
